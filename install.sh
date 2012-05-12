ff24aeda53a4457435624234dece82b09b227762
Jason Frame <jason@onehackoranother.com>
2012-05-12 01:15:45 +0100

    installer

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 cd ~
 
-ln -s .dotfiles/etc/bash_proile     .bash_profile
-ln -s .dotfiles/etc/gitconfig       .gitconfig
-ln -s .dotfiles/etc/gitignore       .gitignore
-ln -s .dotfiles/etc/inputrc         .inputrc
-ln -s .dotfiles/etc/screenrc        .screenrc
-ln -s .dotfiles/etc/irssi           .irssi
+ln -s .dotfiles/etc/bash_proile             .bash_profile
+ln -s .dotfiles/etc/gitconfig               .gitconfig
+ln -s .dotfiles/etc/gitignore               .gitignore
+ln -s .dotfiles/etc/inputrc                 .inputrc
+ln -s .dotfiles/etc/screenrc                .screenrc
+ln -s .dotfiles/etc/irssi                   .irssi
+
+ln -s .dotfiles/etc/vim                     .vim
+ln -s .vim/vimrc-mac                        .vimrc
 
 mkdir -p .ssh
-ln -s Dropbox/dotfiles/ssh_config   .ssh/config
+ln -s Google\ Drive/dotfiles/ssh_config     .ssh/config
+
+mkdir -p ~/tmp
