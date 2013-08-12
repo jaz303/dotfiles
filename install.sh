00392bee795df3e50b889a0acc61aa07ace4f8ae
Jason Frame <jason@onehackoranother.com>
2013-08-12 18:57:36 +0100

    zsh

--- install.sh
+++ install.sh
@@ -7,6 +7,10 @@ ln -s .dotfiles/etc/inputrc                 .inputrc
 ln -s .dotfiles/etc/screenrc                .screenrc
 ln -s .dotfiles/etc/irssi                   .irssi
 
+ln -s .dotfiles/etc/zprofile                .zprofile
+ln -s .dotfiles/etc/zshenv                  .zshenv
+ln -s .dotfiles/etc/zshrc                   .zshrc
+
 ln -s .dotfiles/etc/vim                     .vim
 ln -s .vim/vimrc-mac                        .vimrc
 
