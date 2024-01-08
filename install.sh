2d78b7c4f055076dffdf41455a935fe47ca4f4d7
Jason Frame <jwf@jasonframe.co.uk>
2024-01-08 11:06:04 +0000

    stuff

--- install.sh
+++ install.sh
@@ -1,21 +1,24 @@
 cd ~
 
-ln -s .dotfiles/etc/gitconfig               .gitconfig
-ln -s .dotfiles/etc/gitignore               .gitignore
-ln -s .dotfiles/etc/inputrc                 .inputrc
-ln -s .dotfiles/etc/screenrc                .screenrc
-ln -s .dotfiles/etc/irssi                   .irssi
-ln -s .dotfiles/etc/tmux.conf               .tmux.conf
-ln -s .dotfiles/etc/lldbinit                .lldbinit
-
 ln -s .dotfiles/etc/zprofile                .zprofile
 ln -s .dotfiles/etc/zshenv                  .zshenv
 ln -s .dotfiles/etc/zshrc                   .zshrc
 
+ln -s .dotfiles/etc/gitconfig               .gitconfig
+ln -s .dotfiles/etc/gitignore               .gitignore
+ln -s .dotfiles/etc/screenrc                .screenrc
+ln -s .dotfiles/etc/tmux.conf               .tmux.conf
+
 ln -s .dotfiles/etc/vim                     .vim
 ln -s .vim/vimrc-mac                        .vimrc
 
-mkdir -p .ssh
-ln -s ../Dropbox/dotfiles/ssh_config        .ssh/config
+#ln -s .dotfiles/etc/inputrc                 .inputrc
+#ln -s .dotfiles/etc/irssi                   .irssi
+#ln -s .dotfiles/etc/lldbinit                .lldbinit
+
+mkdir -p .ssh/config.d
+ln -s .dotfiles/etc/ssh/config											.ssh/config
+ln -s .dotifles/etc/ssh/config.d/disable-dhcp-host-key-checking.conf	.ssh/config.d/disable-dhcp-host-key-checking.conf
+chmod -R a=,u=rwX .ssh
 
 mkdir -p ~/tmp
