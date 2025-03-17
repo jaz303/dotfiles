5edc8f2f8a20d775ca7f8648204bdda2090b0c71
Jason Frame <jwf@jasonframe.co.uk>
2025-03-17 18:41:45 +0000

    ok let's try nvim kickstart

---
+++ etc/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,30 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
+  config = function()
+    vim.keymap.set('n', '<C-enter>', ':Neotree show filesystem toggle left<CR>')
+    vim.keymap.set('n', '<C-f>', ':Neotree focus filesystem left<CR>')
+    vim.keymap.set('n', '<C-b>', ':Neotree focus buffers float<CR>')
+  end,
+}
--- etc/nvim/lua/plugins/telescope.lua
+++
@@ -1,4 +0,0 @@
--- plugins/telescope.lua:
-return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' }
-}
